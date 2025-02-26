90c5e69fde021480c9b9cc14f63b9c8578988f63
cziannp <chloep42@nycstudents.net>
2025-02-25 21:49:28 -0500

    Update SortTester.java

--- SortTester.java
+++ SortTester.java
@@ -15,5 +15,44 @@ public class SortTester
     // note the insertionSort method modifies the original array (rather than returning a new array that is sorted)
     Sort.insertionSort(arr2);
     System.out.println(Arrays.toString(arr2));
+
+    
+    /* --- SELECTION SORT --- */
+    int[] arr1 = {100, 89, 86, 77, 54, 40, 19, 14, 13, 12, 6, 3, 0, -4, -5, -20}; 
+    // note the selectionSort method modifies the original array (rather than returning a new array that is sorted)
+    Sort.selectionSort(arr1);
+    System.out.println(Arrays.toString(arr1));
+ 
+    /* --- INSERTION SORT --- */ 
+    int[] arr2 = {100, 89, 86, 77, 54, 40, 19, 14, 13, 12, 6, 3, 0, -4, -5, -20};    
+    // note the insertionSort method modifies the original array (rather than returning a new array that is sorted)
+    Sort.insertionSort(arr2);
+    System.out.println(Arrays.toString(arr2));
+
+
+    /* --- SELECTION SORT --- */
+    int[] arr1 = {-5, -20, 0, -4, 3, 13, 6, 12, 14, 40, 19, 54, 77, 89, 100, 86}; 
+    // note the selectionSort method modifies the original array (rather than returning a new array that is sorted)
+    Sort.selectionSort(arr1);
+    System.out.println(Arrays.toString(arr1));
+ 
+    /* --- INSERTION SORT --- */ 
+    int[] arr2 = {-5, -20, 0, -4, 3, 13, 6, 12, 14, 40, 19, 54, 77, 89, 100, 86};    
+    // note the insertionSort method modifies the original array (rather than returning a new array that is sorted)
+    Sort.insertionSort(arr2);
+    System.out.println(Arrays.toString(arr2));
+
+
+    /* --- SELECTION SORT --- */
+    int[] arr1 = {-20, -5, -4, 0, 3, 6, 12, 13, 14, 19, 40, 54, 77, 86, 89, 100}; 
+    // note the selectionSort method modifies the original array (rather than returning a new array that is sorted)
+    Sort.selectionSort(arr1);
+    System.out.println(Arrays.toString(arr1));
+ 
+    /* --- INSERTION SORT --- */ 
+    int[] arr2 = {-20, -5, -4, 0, 3, 6, 12, 13, 14, 19, 40, 54, 77, 86, 89, 100};    
+    // note the insertionSort method modifies the original array (rather than returning a new array that is sorted)
+    Sort.insertionSort(arr2);
+    System.out.println(Arrays.toString(arr2));
   }
 }
